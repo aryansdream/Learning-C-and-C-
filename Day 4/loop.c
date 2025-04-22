c3363fbedd9201ee2e65800c9b0b8716892393d5
Aryan Tiwari <154964729+aryansdream@users.noreply.github.com>
2025-04-22 12:50:57 +0530

    Add files via upload

---
+++ Day 4/loop.c
@@ -0,0 +1,15 @@
+/* 
+FOR LOOP
+#include <stdio.h>
+int main(){
+    for(initialization; condition; increament){
+        statement}
+        return 0;
+    }                                       
+example */
+#include <stdio.h>
+int main(){
+for(int i = 1; i<=5; i++){
+    printf("Hey This is Aryan \n");
+}
+}
